b499742c85ff434269ff406edf556c83228e80cb
hoadao <hoa.thanh.dao@logigear.com>
2019-11-14 10:47:48 +0700

    update data of sample test case

--- src/tests/addProject_test.ts
+++ src/tests/addProject_test.ts
@@ -11,7 +11,7 @@ TestCase("Verify add project successfully", async () => {
     await loginPage.login(constants.adminUserName, constants.adminPassword);
     await loginPage.gotoBusinessSystem();
     await businessSystempage.gotoAddProjectPage();
-    await addProjectPage.inputProjectOverview("123", constants.projectForms.result, "TEST", "営業部", "西川三郎", "2019-08-13", "2019-08-16", "2019-08-13", "2019-08-16", constants.accuracyTypes.high,
+    await addProjectPage.inputProjectOverview("123", constants.projectForms.result, "test", "営業部", "西川三郎", "2019-08-13", "2019-08-16", "2019-08-13", "2019-08-16", constants.accuracyTypes.high,
         constants.projectStatuses.delivered, constants.projectPlace.dispatch, constants.currencyIds.jpy, constants.billingTypes.consolidate, constants.closingDates[1], "123", "123", "123");
     await addProjectPage.inputProjectResultBases(constants.projectRole.designer, "SES", constants.debitCreditGroupIds.advance, 2, 8, "300", "3000", "4000", "5000", "5000", "5000", "7000", true, constants.taxIds[10], "123", "4");
     await addProjectPage.addProjectDetails("1", "abc", "SES", constants.debitCreditGroupIds.advance, true, constants.taxIds[10], "1", "1", "1", "2019-08-13", "2019-08-16", "2019-08-17", "2019-08-18");
